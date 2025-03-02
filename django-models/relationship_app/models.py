ee974a466a758edba52a46bec02d9e2d3ed0589c
Atlas00000 <emilicelestine@gmail.com>
2025-03-02 10:18:30 +0100

    Initial commit

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,32 +1,28 @@
 from django.db import models
 
-# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=255)
-
+    
     def __str__(self):
         return self.name
 
-# Book Model (ForeignKey to Author)
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
     def __str__(self):
         return self.title
 
-# Library Model (ManyToMany with Book)
 class Library(models.Model):
     name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book, related_name="libraries")
-
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
     def __str__(self):
         return self.name
 
-# Librarian Model (OneToOne with Library)
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
-
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    
     def __str__(self):
         return self.name
